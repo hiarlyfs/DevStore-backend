d9270ce5bbda8878886c8892a3c4365d936b1748
Hiarly Fernandes <hiarly.souto@ccc.ufcg.edu.br>
2020-09-07 08:28:38 -0300

    Atualiza porta do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
 import app from './app'
 
-app.listen(process.env.PORT | 3333)
+const port = process.env.PORT || 3333
+
+app.listen(port, () => {
+  console.log('Server listening on port ' + port)
+})
